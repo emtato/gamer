24c9e6060a5ed3568d2b7201fbed77972ca442ad
amandali <amanda.jianing.li@gmail.com>
2024-08-30 15:23:03 -0400

    hi

---
+++ Bullet.py
@@ -0,0 +1,10 @@
+# Description:
+# Created by Emilia and Amanda on 2024-08-30
+import sys
+import pygame
+import time
+import random
+
+class Bullet:
+    def __init__(self, element):
+        self.element = element
