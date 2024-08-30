e57b9758c5471e6689317bcc2720874812729649
Emilia <emiliama2006@gmail.com>
2024-08-30 12:14:06 -0400

    meow 2

---
+++ Main.py
@@ -0,0 +1,13 @@
+# Description:
+# Created by Emilia and Amanda on 2024-08-30
+import sys
+import pygame
+import time
+import random
+
+Width, Height = 1400, 1000
+Window = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption("qwack")
+
+#main game logic, while loop to run everything
+def main():
--- test.py
+++
@@ -1,9 +0,0 @@
-# Description:
-# Created by Emilia on 2024-08-30
-# pr - pl - rdi - rdii - rd - for - for2 - form - fors - forl - base - rar - ar - ar2 - isp - 2dl - rio - map - dq r2i
-import sys
-
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-
