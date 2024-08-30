b248fde607edb38de524feb5ae9fcb687378709c
Emilia Ma <emiliama2006@gmail.com>
2024-08-30 11:51:44 -0400

    meow

---
+++ test.py
@@ -0,0 +1,13 @@
+# Description:
+# Created by Emilia on 2024-08-30
+# pr - pl - rdi - rdii - rd - for - for2 - form - fors - forl - base - rar - ar - ar2 - isp - 2dl - rio - map - dq r2i
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+ar = []
+ar = list(map(int, input().split()))
+for i in range(n):
+    a
