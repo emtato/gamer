255924e6e9719ee97d3532cafe0652e3de5800df
Emilia <emiliama2006@gmail.com>
2024-09-03 16:24:03 -0400

    test

--- Level.py
+++ Level.py
@@ -1,4 +1,4 @@
-# Description:
+# Description: MAKE SAVE FILES LATER!!!!! replace "filename, r" with 'w' or 'a' in a new file
 # Created by Emilia and Amanda on 2024-08-30
 import sys
 import pygame
@@ -13,7 +13,7 @@ class Level:
         file = open(fileName, "r")
         for i in range(levelNum-1):
             file.readline()
-        List = file.readLine().split()
+        List = map(int, file.readLine().split())
         self.image = List[0]
         self.playerX = List[1]
         self.playerY = List[2]
